77214fae1bb019bfbc38d4774eb6aecdd8adb75f
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-08-13 21:49:27 +0200

    link from router

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./UserItem.css";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import { Link } from "react-router-dom";
 const UserItem = ({ user }) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={user.image} alt="avatar pic" alt={user.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{user.name}</h2>
-          <h3>
-            {user.placeCount} {user.placeCount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
+        <Link to={`${user.name}/places`}>
+          <div className="user-item__image">
+            <Avatar image={user.image} alt={user.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{user.name}</h2>
+            <h3>
+              {user.placeCount} {user.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
       </div>
     </li>
   );
